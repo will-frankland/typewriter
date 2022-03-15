8a87c0be1bccac08e66615a76e093e70403740c9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-03-15 13:46:31 -0400

    initial commit

---
+++ Spinner/spinner2.js
@@ -0,0 +1,49 @@
+// Refactor code below to remove the repeated timeout statements.
+
+// group symbols in an array
+// for loop to loop symbols
+// setTimeout inside loop (+=) current delay + 200ms
+
+let delay = 0;
+let symbol = ["|", "/", "-", "\\"];
+for (elem of symbol) {
+  setTimeout(() => 
+    process.stdout.write(elem), delay)
+  delay += 200
+};
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1700);
